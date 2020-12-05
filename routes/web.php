bc35c2b452ee4a5eceab8cc6c4430936027fface
Nicolás Linares La Barba <nico@MacBook-Air-de-Nicolas.local>
2020-12-05 19:57:41 +0100

    Implementing refresh token

--- routes/web.php
+++ routes/web.php
@@ -16,64 +16,53 @@
 
 use Illuminate\Support\Facades\Storage;
 
-$router->get('storage/{directory}/{filename}', function ($directory, $filename)
-    {
-        return Storage::download($directory.'/'.$filename);             // Download file
-    }
-);
+$router->get('storage/{directory}/{filename}', function ($directory, $filename) {
+    return Storage::download($directory . '/' . $filename);             // Download file
+});
 
 // https://pln.inf.um.es/TFG_MobileApp_API/public/storage/24/file.m4a
-$router->get('storage/{directory}/{filename}', function ($directory, $filename)
-    {
-        Storage::disk('local')->delete($directory.'/'.$filename);           // Delete specific file
-    }
-);
+$router->get('storage/{directory}/{filename}', function ($directory, $filename) {
+    Storage::disk('local')->delete($directory . '/' . $filename);           // Delete specific file
+});
 
 
 // https://pln.inf.um.es/TFG_MobileApp_API/public/delete/24
-$router->get('delete/{dir}', function ($dir)
-    {
-        Storage::disk('local')->deleteDirectory($dir);              // Delete all in dir
-    }
-);
+$router->get('delete/{dir}', function ($dir) {
+    Storage::disk('local')->deleteDirectory($dir);              // Delete all in dir
+});
 
 
 // https://pln.inf.um.es/TFG_MobileApp_API/public/exists/24/file.m4a
-$router->get('exists/{directory}/{filename}', function ($directory, $filename)
-    {
-        $value = Storage::disk('local')->exists($directory.'/'.$filename);      // Exists
-        return response()->json($value, 200);
-    }
-);
+$router->get('exists/{directory}/{filename}', function ($directory, $filename) {
+    $value = Storage::disk('local')->exists($directory . '/' . $filename);      // Exists
+    return response()->json($value, 200);
+});
 
 
 // https://pln.inf.um.es/TFG_MobileApp_API/public/rename/24/file.m4a
-$router->get('rename/{directory}/{filename}', function ($directory, $filename)
-    {
-        Storage::disk('local')->move($directory.'/'.$filename, $directory.'/renombrado.m4a');   // renombrar
-    }
-);
+$router->get('rename/{directory}/{filename}', function ($directory, $filename) {
+    Storage::disk('local')->move($directory . '/' . $filename, $directory . '/renombrado.m4a');   // renombrar
+});
 
 
 
 
 
-$router->group(['prefix' => 'v1'], function($router)
-{
+$router->group(['prefix' => 'v1'], function ($router) {
     // LOGIN
-    $router->post('/auth/login',  ['uses' => 'AuthController@login']);
+    $router->post('/login',  ['uses' => 'AuthController@login']);
     // SIGNUP
-    $router->post('/auth/signin',  ['uses' => 'AuthController@signin']);
-    // REFRESH
-    $router->put('/auth/refresh',  ['uses' => 'AuthController@refresh']);
+    $router->post('/signin',  ['uses' => 'AuthController@signin']);
 
 
     // Todas las peticiones que se producen dentro de la app pasan a través
     // de un middleware de autenticación JWT
-    $router->group(['middleware' => 'auth:api'], function($router)
-    {   
+    $router->group(['middleware' => 'auth:api'], function ($router) {
+        // REFRESH
+        $router->put('/refresh',  ['uses' => 'AuthController@refresh']);
+
         // LOGOUT
-        $router->delete('/auth/logout', ['uses' => 'AuthController@logout']);
+        $router->delete('/logout', ['uses' => 'AuthController@logout']);
 
         // USER OPS
         $router->get('users', ['uses' => 'UsersController@getAll']);
@@ -93,9 +82,8 @@ $router->group(['prefix' => 'v1'], function($router)
         $router->post('audio', ['uses' => 'AudiosController@add']);
         $router->get('audio/{uid}', ['uses' => 'AudiosController@downloadAudioFile']);
         $router->delete('audio/{uid}', ['uses' => 'AudiosController@delete']);
-        
+
         $router->put('audio/description/{uid}', ['uses' => 'AudiosController@updateDescription']);
         $router->put('audio/name/{uid}', ['uses' => 'AudiosController@updateName']);
-
     });
 });
