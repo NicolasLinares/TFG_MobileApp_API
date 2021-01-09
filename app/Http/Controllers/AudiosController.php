2a13236f8373b95313fa5dddad594aff8d387f57
Nicolás Linares La Barba <nico@MacBook-Air-de-Nicolas.local>
2021-01-10 00:10:35 +0100

    Testing transcript

--- app/Http/Controllers/AudiosController.php
+++ app/Http/Controllers/AudiosController.php
@@ -143,7 +143,7 @@ class AudiosController extends Controller
                 ->get();;
             
             $transcript = Transcript::select('text')
-                ->where('id_audio', strval($id_audio))
+                ->where('id_audio', $id_audio)
                 ->get();
 
             return response()->json($transcript, 200);
