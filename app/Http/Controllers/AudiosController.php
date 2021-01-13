945c167fdb3a4b44b0d5efe1a744b99176bbbaef
Nicolás Linares La Barba <nico@MacBook-Air-de-Nicolas.local>
2021-01-13 23:20:07 +0100

    Testing jobs

--- app/Http/Controllers/AudiosController.php
+++ app/Http/Controllers/AudiosController.php
@@ -34,7 +34,7 @@ class AudiosController extends Controller
                 ->join('transcript', 'audio.id', '=', 'transcript.id_audio')
                 ->get(['audio.*', 'transcript.text as transcription', 'transcript.status']);
 
-            $paginated = new Paginator($data, 10);
+            $paginated = new Paginator($data, 10, 2);
             return response()->json($paginated->toArray(), 200);
         } else {
             return response()->json(['error' => 'Usuario no autorizado.'], 401);
@@ -56,7 +56,7 @@ class AudiosController extends Controller
                 ->join('transcript', 'audio.id', '=', 'transcript.id_audio')
                 ->get(['audio.*', 'transcript.text as transcription', 'transcript.status']);
 
-            $paginated = new Paginator($data, $data->count(), 10);
+            $paginated = new Paginator($data, 10);
             return response()->json($paginated->toArray(), 200);
         } else {
             return response()->json(['error' => 'Usuario no autorizado.'], 401);
@@ -78,7 +78,7 @@ class AudiosController extends Controller
                 ->join('transcript', 'audio.id', '=', 'transcript.id_audio')
                 ->get(['audio.*', 'transcript.text as transcription', 'transcript.status']);
 
-            $paginated = new Paginator($data, $data->count(), 10);
+            $paginated = new Paginator($data, 10);
             return response()->json($paginated->toArray(), 200);
         } else {
             return response()->json(['error' => 'Usuario no autorizado.'], 401);
